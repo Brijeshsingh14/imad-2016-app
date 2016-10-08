91099c94afac8db0a791bc053740f841d3943713
Brijeshsingh14 <selectbrijesh@gmail.com>
2016-10-08 09:12:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/counter', function (req, res){
   res.send(counter.toString());  
     
 }
-
+);
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
